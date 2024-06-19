594d83e512560ac91658af03a32992fc7e252bcf
ARithvikReddy <arithvikreddy105@gmail.com>
2024-06-19 10:44:31 +0530

    7

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -3,9 +3,8 @@ import React from 'react'
 const login = () =>{
     return (
         <div>
-            this is login page made by rithvik
-            THIS IS LOGIN OAGE MADE BY ANISH
-            try 3
+            this is login
+            THIS IS LOGIN OAGE
             
         </div>
     )
